5c481535506d2bd698164890bdccda73271eeb79
Zachary Li Norman <zacharylinorman@gmail.com>
2019-10-31 05:29:16 -0700

    almost there

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,18 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+		$("#scrollBar").css({opacity:$(window).scrollTop()/(window.innerHeight*0.925)});
+		if($(window).scrollTop()==0){
+			$("#scrollBar").css({display:"none"});
+		}else{
+			$("#scrollBar").css({display:"block"});
+		}
+    });
+
+});
+function srl(div){
+	$(window).scrollTo($('#'+div), 800, {offset: -window.innerHeight*0.0749});
+}
 function openInNewTab(url){
 	window.open(url, '_blank');
-}
-function scroll(div){
-	$(window).scrollTo($('#'+div), 800, {offset: -25%});
 }
